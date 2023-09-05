e57ce77dd239fe5d2196acb15d82b2f61927f48a
Tobias Ahrens <tobias-ahrens@hotmail.de>
2023-09-05 16:15:47 +0200

    about page unfinished

---
+++ components/about.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+        <h2 className='text-3xl mb-8 font-bold'>
+            ABOUT ME
+        </h2>
+        <p className='mb-3'>
+            This is the first one asdhasdhashfahsghadghsdghsdhfashfd ahsdfhasdh ahsdhasdh asdhas dhas dh
+        </p>
+
+        <p>
+            this is the second one
+        </p>
+    </section>
+  )
+}
